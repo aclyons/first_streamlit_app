9180b84655d7280c1f1a96ff1e55a5364635cf55
ACLyons <93389529+aclyons@users.noreply.github.com>
2023-10-06 13:59:56 -0700

    Update streamlit_app.py
    Add a second text entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,8 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_row)
+
+#New Section to display fruityvice api response
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', fruit_choice)
+
